02902875cf11cb9882752032d33e8a492ca084f3
denysV1978 <denysvuk@gmail.com>
2019-11-16 00:04:10 -0600

    Studio for Classes Part 1 is changed

--- src/org/launchcode/java/RestaurantMenu/restaurant/Menu.java
+++ src/org/launchcode/java/RestaurantMenu/restaurant/Menu.java
@@ -5,14 +5,34 @@ import java.util.Map;
 
 public class Menu {
 
-    private HashMap<Integer, Object> menuList = new HashMap<>();
+    private HashMap<Integer, MenuItem> menuList = new HashMap<>();
 
 
-    public void Menu(int number, Object menuItem) {
+    public void Menu(int number, MenuItem menuItem) {
 
         menuList.put(number, menuItem);
 
     };
+
+    public void getItems() {
+
+        for(MenuItem item : this.menuList.values()) {
+
+            System.out.println(item.getMenuItemName() + "\t" + item.getMenuItemCategory());
+        };
+
+    };
+
+    public void getItems2() {
+
+        for (Map.Entry<Integer, MenuItem> item : this.menuList.entrySet()) {
+
+            System.out.println(item.getValue().getMenuItemName() + "\t" + item.getValue().getMenuItemCategory());
+        }
+        ;
+
+    }
+
 
 
 }
--- src/org/launchcode/java/RestaurantMenu/restaurant/Operation.java
+++ src/org/launchcode/java/RestaurantMenu/restaurant/Operation.java
@@ -8,10 +8,16 @@ public class Operation {
     public static void main(String[] args) {
 
         MenuItem menuItem1 = new MenuItem();
+        MenuItem menuItem2 = new MenuItem();
         menuItem1.MenuItem("Tomato soup", "soup from tomatoes", "Soups", 5.00, true, "11/14/2019");
+        menuItem2.MenuItem("Potato soup", "soup from potatoes", "Soups", 5.00, true, "11/14/2019");
 
         Menu menuNovember = new Menu();
         menuNovember.Menu(1, menuItem1);
+        menuNovember.Menu(2,menuItem2);
+
+        menuNovember.getItems();
+        menuNovember.getItems2();
 
 
 
