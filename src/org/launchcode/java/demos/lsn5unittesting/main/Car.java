a38f54def3f56a7bb5212dc00de389e40b377c6a
denysV1978 <denysvuk@gmail.com>
2019-11-24 22:18:35 -0600

    chapter6ExercisesIsDone

--- src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
+++ src/org/launchcode/java/demos/lsn5unittesting/main/Car.java
@@ -49,7 +49,7 @@ public class Car {
     public void setGasTankLevel(double gasTankLevel) {
         if (gasTankLevel > this.getGasTankSize()) {
             throw new IllegalArgumentException("Can't exceed tank size");
-        };
+        }
         this.gasTankLevel = gasTankLevel;
     }
 
@@ -86,7 +86,7 @@ public class Car {
         double gallonsUsed = milesAbleToTravel / this.milesPerGallon;
         this.gasTankLevel = this.gasTankLevel - gallonsUsed;
         this.odometer += milesAbleToTravel;
-    };
+    }
 
     public void addGas(double gas) {
         this.setGasTankLevel(gas + this.getGasTankLevel());
